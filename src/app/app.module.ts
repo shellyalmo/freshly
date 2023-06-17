38fe75d57c9c0fa7a3c1cc7db1b8303bc42b6d1f
shellyalmo <shellyalmo@gmail.com>
2023-06-17 20:03:32 +0300

    feat: foods data from local json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
@@ -16,7 +16,8 @@ import { FoodsComponent } from './foods/foods.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+
 type food = { foodItemId:string, foodItemName: string, expirationDate: Date }
 
 @Component({
@@ -6,11 +8,16 @@ type food = { foodItemId:string, foodItemName: string, expirationDate: Date }
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.scss']
 })
+
 export class FoodListComponent {
   public foods: food[] = [
     { foodItemId: crypto.randomUUID(), foodItemName: 'instant noodles', expirationDate: new Date('2020-12-31') },
     { foodItemId: crypto.randomUUID(), foodItemName: 'instant noodles', expirationDate: new Date('2023-12-31') },
   ]
+
+  public searchResult: string =""
+
+  constructor(private http: HttpClient){}
 
   public deleteFood(foodItem: food) {
      this.foods = this.foods.filter(food => (food.foodItemId!==foodItem.foodItemId))
@@ -22,5 +29,16 @@ export class FoodListComponent {
     }
     this.foods.push({ foodItemId: crypto.randomUUID(),foodItemName: newFood, expirationDate: newDate })
     this.foods.sort((a, b) => {return a.expirationDate.getTime() - b.expirationDate.getTime()})
+  }
+
+  public searchFood(foodToSearch: string){
+    foodToSearch=foodToSearch.charAt(0).toUpperCase()+foodToSearch.slice(1);
+    const queryParams = new HttpParams().set('name',foodToSearch);
+    const url = 'http://localhost:3000/product_data';
+
+    this.http.get<any[]>(url, { params: queryParams })
+    .subscribe((data: any[]) => {
+      this.searchResult = data[0].refrigerate_after_opening_output_display_only;
+    })
   }
 }
